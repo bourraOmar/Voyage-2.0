8a942bae7847c926899c99067e704bd4e6a621e0
WissamDouskary <douskary.wissam@gmail.com>
2024-12-26 10:31:13 +0100

    Upgrade Tables on Script SQL

--- connection/scripte.sql
+++ connection/scripte.sql
@@ -2,48 +2,36 @@ CREATE DATABASE voyage_2;
 
 USE voyage_2;
 
-CREATE TABLE client(
-  idClient int NOT NULL,
+CREATE TABLE role(
+  role_id INT AUTO_INCREMENT PRIMARY KEY,
+  titre VARCHAR(225)
+);
+
+CREATE TABLE User(
+  User_id INT PRIMARY KEY AUTO_INCREMENT,
   nom VARCHAR(100),
   prenom VARCHAR(100),
   email VARCHAR(100),
   password VARCHAR(100),
-  status enum('active','blocke'),
-  PRIMARY KEY (idClient)
-);
-
-CREATE TABLE admin(
-  idAdmin int NOT NULL,
-  nom VARCHAR(100),
-  prenom VARCHAR(100),
-  email VARCHAR(100),
-  password VARCHAR(100),
-  PRIMARY KEY (idAdmin)
-);
-
-CREATE TABLE superAdmin(
-  idSuperAdmin int NOT NULL,
-  nom VARCHAR(100),
-  prenom VARCHAR(100),
-  email VARCHAR(100),
-  password VARCHAR(100),
-  PRIMARY KEY (idSuperAdmin)
-);
-
-CREATE TABLE reservation(
-  idReservation int NOT NULL,
-  date_activite datetime,
-  status enum('waiting','accepte','refuse'),
-  PRIMARY KEY (idActivity)
+  status ENUM('active', 'blocked'),
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(role_id)
 );
 
 CREATE TABLE activity(
-  idActivity int NOT NULL,
+  activity_id INT AUTO_INCREMENT PRIMARY KEY,
   nom VARCHAR(100),
   description VARCHAR(100),
-  price float,
-  date_activite datetime,
-  idReservation int,
-  PRIMARY KEY (idActivity),
-  FOREIGN KEY (idReservation) REFERENCES reservation(idReservation)
+  price DECIMAL(10, 2),
+  date_activite DATETIME
+);
+
+CREATE TABLE reservation(
+  reservation_id INT AUTO_INCREMENT PRIMARY KEY,
+  date_activite DATETIME,
+  status ENUM('waiting', 'accepte', 'refuse'),
+  User_id INT,
+  activity_id INT,
+  FOREIGN KEY (User_id) REFERENCES User(User_id),
+  FOREIGN KEY (activity_id) REFERENCES activity(activity_id)
 );
